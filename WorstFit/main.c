f4f2777191e3ed63cb54dd2b8da6fab8fb5e8d7d
paulinarubio <Paulina@Paulinas-MacBook-Pro.local>
2013-10-20 22:15:40 -0700

    Initial Commit

---
+++ WorstFit/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  WorstFit
+//
+//  Created by Paulina Rubio Tarriba on 10/20/13.
+//  Copyright (c) 2013 Paulina Rubio Tarriba. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
